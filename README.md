1d881fe519e417f7d1ec05b4c2caed7317d1a0b1
br4in3x <max.a.pimenov@gmail.com>
2019-08-28 11:24:01 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Email template boilerplate
+
+This project can help you to kick start an email template layout. 
+
+Templates located at `/templates` folder, and each template has it's own .css file. Change the template layout and css file, and run `npm run build` command to get your email template built with css inlined.
+
+Alternatively, you can run `npm start` command to see your changes in browser. While `npm start` is running, open the template file from `build` folder in the browser and modify files within this folder. To see your changes reload the browser.
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require("gulp");
+const nj = require("gulp-nunjucks-render");
+const inline_source = require("gulp-inline-source");
+const inline_css = require("gulp-inline-css");
+
+function build() {
+  return gulp
+    .src("./templates/**/*.nj")
+    .pipe(nj())
+    .pipe(
+      inline_source({
+        rootpath: __dirname
+      })
+    )
+    .pipe(
+      inline_css({
+        applyLinkTags: false,
+        removeLinkTags: true
+      })
+    )
+    .pipe(gulp.dest("./build"));
+}
+
+function watch() {
+  return gulp.watch(["./templates/**/*.nj", "./lib"], build);
+}
+
+exports.build = build;
+exports.watch = gulp.series(build, watch);
